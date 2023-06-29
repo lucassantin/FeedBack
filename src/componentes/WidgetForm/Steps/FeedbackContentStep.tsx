319e6d6c9c0979e4878be975be40b9f727090040
Lucas <lucasdossantossantin@gmail.com>
2023-06-29 00:19:50 -0300

    textarea

--- src/componentes/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/componentes/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -35,9 +35,13 @@ export function FeedbackContentStep({
         <CloseButton />
       </header>
 
-      <div className="flex py-8 gap-2 w-full">
-        por enquanto esse conteúdo está vazio
-      </div>
+      <form className="my- w-full">
+        <textarea
+          className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 
+          text-zinc-100 border-zinc-600 bg-transparent rounded-md"
+          placeholder="Conte com detalhes o que está acontecendo..."
+        />
+      </form>
     </>
   );
 }
